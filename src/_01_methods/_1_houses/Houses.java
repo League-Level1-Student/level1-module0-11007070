b29c93c5af69f67bb2ddae2690f74ea92d19146f
root <root@9c3efe418f72>
2021-04-21 23:56:40 +0000

    4/21/21

--- src/_01_methods/_1_houses/Houses.java
+++ src/_01_methods/_1_houses/Houses.java
@@ -1,11 +1,19 @@
 package _01_methods._1_houses;
+
+import java.awt.Robot;
+import java.nio.charset.MalformedInputException;
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2021
  *    Level 1
  */
 public class Houses {
 		
+	public static void main(String[] args) {
+		Robot rob = new Robot();
+	}
 	public void run() {
+		
 		// Check the recipe to find out what code to put here
 	}
 }
