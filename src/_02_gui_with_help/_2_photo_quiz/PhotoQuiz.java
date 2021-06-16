f8cbfa4ae1a6b5a4db3429e90f9bddda60c95a1b
root <root@22ee8c57f797>
2021-06-16 23:55:38 +0000

    6/16/21

--- src/_02_gui_with_help/_2_photo_quiz/PhotoQuiz.java
+++ src/_02_gui_with_help/_2_photo_quiz/PhotoQuiz.java
@@ -35,29 +35,50 @@ public class PhotoQuiz {
 		comp = createImage(url);
 
 		// 4. add the image to the quiz window
-		
+		quizWindow.add(comp);
 
 		// 5. call the pack() method on the quiz window
+		quizWindow.pack();
 
 		// 6. ask a question that relates to the image
+		String answer = JOptionPane.showInputDialog("Is this image a sunset?");
 
 		// 7. print "CORRECT" if the user gave the right answer
+		if (answer.equalsIgnoreCase("yes")) {
+			System.out.println("CORRECT");
+		}
 
 		// 8. print "INCORRECT" if the answer is wrong
+		else if (answer.equalsIgnoreCase("no")) {
+			System.out.println("INCORRECT");
+		}
 
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
+		quizWindow.remove(comp);
 
 		// 10. find another image and create it (might take more than one line
 		// of code)
-
+		String url1 = "https://watchandlearn.scholastic.com/content/dam/classroom-magazines/watchandlearn/videos/earth-and-space/earth-science-and-space/what-s-in-the-night-sky/english/wall-2018-whatsinthenightskymp4.transform/content-tile-large/image.png";
+		Component comp1;
+		comp1 = createImage(url1);
+		
 		// 11. add the second image to the quiz window
+		quizWindow.add(comp1);
 
 		// 12. pack the quiz window
+		quizWindow.pack();
 
 		// 13. ask another question
+		String answer1 = JOptionPane.showInputDialog("Is this image of a night sky?");
 
 		// 14+ check answer, say if correct or incorrect, etc.
+		if (answer1.equalsIgnoreCase("yes")) {
+			System.out.println("CORRECT");
+		}
+		else if (answer1.equalsIgnoreCase("no")) {
+			System.out.println("INCORRECT");
+		}
 
 	}
 
--- src/_02_gui_with_help/_3_drum_kit/DrumKit.java
+++ src/_02_gui_with_help/_3_drum_kit/DrumKit.java
@@ -14,8 +14,10 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class DrumKit implements MouseListener {
 	static boolean canPlaySounds = true; // Set this to false if your computer cannot play sounds
@@ -24,31 +26,42 @@ public class DrumKit implements MouseListener {
 	public void run() {
 
 		//  Make a JFrame variable and initialize it using "new JFrame()"
-
+		JFrame frame = new JFrame();
+		
 		//  Make the frame visible and
 		// set its default close operation to JFrame.EXIT_ON_CLOSE
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		//  Set the title of the frame
+		frame.setTitle("Drum Kit");
 
 		//  Make a JPanel variable and initialize it using "new JPanel().
-
+		JPanel panel = new JPanel();
+		
 		//  Add the panel to the frame. (The panel is invisible.)
+		panel.setVisible(true);
+		frame.add(panel);
 
 		//  Use the image of a drum provided ("snare.jpg") or
 		//  download an image of a drum from the Internet and drop it into the
 		//  Drum Kit recipe package.
 
 		//  Put the name of the drum image file in a String variable.
+		String image = "snare.jpg";
 
 		//  Edit the next line to use your String variable
-		// drumLabel = createLabelImage(drumImageString);
+		drumLabel = createLabelImage(image);
 
 		//  Add the label to the panel
+		panel.add(drumLabel);
 
 		//  Call the pack() method on the frame. 
 		// Run your program. Do you see your drum image?
+		frame.pack();
 
 		// Add this MouseListener to drumLabel
+		
 		
 		// *** Write the code in the mouseClicked() method below
 
